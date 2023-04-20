eae93ffa3741ba4fabc7bc7b0fb30e9d9bbd4df1
Ivan Korolev <ikorolev13@gmail.com>
2023-04-20 11:03:24 +0300

    IKorolev/ junit4-platform/ add two class with JUnit4 and Jupiter

--- src/test/java/ru/netology/service/CashbackHackServiceJUnit4Test.java
+++ src/test/java/ru/netology/service/CashbackHackServiceJUnit4Test.java
@@ -3,7 +3,7 @@ package ru.netology.service;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-public class CashbackHackServiceTest {
+public class CashbackHackServiceJUnit4Test {
 
     @Test
     public void testMinSumPurchase() {
